2b3a286f79073d63037ff27a549cca5db8e9713a
picompute4-smartmeter <jazzpiano1004@gmail.com>
2021-05-26 08:51:35 +1000

    update cs5484_wiringpi by add start_conversion, read i, v etc.

--- cs5484_wiringpi.c
+++ cs5484_wiringpi.c
@@ -101,11 +101,123 @@ uint8_t reset(uint8_t csum_en)
 {
     return instruction(0x01, csum_en);
 }
-/*
-uint8_t start_coversion(uint8_t conversion_type, uint8_t csum_en)
-{
-    setpage(0, csum_en);
-    write(0x800000, 23, csum_en);
 
+uint8_t start_conversion(uint8_t conversion_type, uint8_t csum_en, int timeout)
+{   
+    page_select(0, csum_en); // set to page 0
+    reg_write(0x800000, 23, csum_en); // set Interrupt Status (Status0) to default
+    
+    if(conversion_type == CONVERSION_TYPE_SINGLE)          instruction(0x14, csum_en); // set single conversion 
+    else if(conversion_type == CONVERSION_TYPE_CONTINUOUS) instruction(0x15, csum_en); // set continuous conversion
+    else return STATUS_FAIL;
+
+    uint32_t status0;
+    uint8_t ret;
+    int timeout_cnt=timeout;
+    int ready=0;
+    while(!ready)
+    {
+	ret = reg_read(&status0, 23, csum_en);
+	//printf("read status0 : %x\r\n", status0);
+	if(ret == STATUS_OK)
+	{
+            // data ready and conversion ready flag in status0
+	    if((status0 & 0xC00000) == 0xC00000)
+	    {   
+                page_select(0, 0);
+		reg_write(0x800000, 23, 0);
+		ready = 1;		
+	    }
+	    else{
+		timeout_cnt--;
+	    }
+	}
+	else{
+	    timeout_cnt--;
+	}
+
+	// check timeout
+	if(timeout_cnt <= 0){
+	    return STATUS_TIMEOUT;
+	}
+
+	delay(1);
+    }
+
+    return STATUS_OK;
 }
-*/
+
+uint32_t get_voltage_peak(uint8_t input_channel, uint8_t csum_en)
+{
+    uint8_t addr;
+    uint32_t v;
+
+    if(input_channel == ANALOG_INPUT_CH1)   	addr = 36;
+    else if(input_channel == ANALOG_INPUT_CH2)	addr = 38;
+    else return STATUS_FAIL;
+
+    page_select(0, csum_en);
+    reg_read(&v, addr, csum_en);
+
+    return v;
+}
+
+uint32_t get_current_peak(uint8_t input_channel, uint8_t csum_en)
+{
+    uint8_t addr;
+    uint32_t i;
+
+    if(input_channel == ANALOG_INPUT_CH1)   	addr = 37;
+    else if(input_channel == ANALOG_INPUT_CH2)	addr = 39;
+    else return STATUS_FAIL;
+
+    page_select(0, csum_en);
+    reg_read(&i, addr, csum_en);
+
+    return i;
+}
+
+uint32_t get_voltage_rms(uint8_t input_channel, uint8_t csum_en)
+{
+    uint8_t addr;
+    uint32_t v;
+
+    if(input_channel == ANALOG_INPUT_CH1)   	addr = 7;
+    else if(input_channel == ANALOG_INPUT_CH2)	addr = 13;
+    else return STATUS_FAIL;
+
+    page_select(16, csum_en);
+    reg_read(&v, addr, csum_en);
+
+    return v;
+}
+
+uint32_t get_current_rms(uint8_t input_channel, uint8_t csum_en)
+{
+    uint8_t addr;
+    uint32_t i;
+
+    if(input_channel == ANALOG_INPUT_CH1)   	addr = 6;
+    else if(input_channel == ANALOG_INPUT_CH2)	addr = 12;
+    else return STATUS_FAIL;
+
+    page_select(16, csum_en);
+    reg_read(&i, addr, csum_en);
+
+    return i;
+}
+
+uint32_t get_power_avg(uint8_t input_channel, uint8_t csum_en)
+{
+    uint8_t addr;
+    uint32_t p;
+
+    if(input_channel == ANALOG_INPUT_CH1)   	addr = 5;
+    else if(input_channel == ANALOG_INPUT_CH2)	addr = 11;
+    else return STATUS_FAIL;
+
+    page_select(16, csum_en);
+    reg_read(&p, addr, csum_en);
+
+    return p;
+}
--- cs5484_wiringpi.h
+++ cs5484_wiringpi.h
@@ -7,15 +7,32 @@
 #include <wiringPiSPI.h>
 #include <wiringPi.h>
 
-#define SPI_CHANNEL 0
-#define CS_PIN 5
-#define STATUS_OK 0
-#define STATUS_FAIL 1
+#define SPI_CHANNEL 	0
+#define CS_PIN 		5
+
+#define STATUS_OK	0
+#define STATUS_FAIL 	1
+#define STATUS_TIMEOUT 	2
+
+#define CONVERSION_TYPE_SINGLE 	   1
+#define CONVERSION_TYPE_CONTINUOUS 2
+
+#define ANALOG_INPUT_CH1 1
+#define ANALOG_INPUT_CH2 2
+
 
 uint8_t reg_write(uint32_t data, uint8_t addr, uint8_t csum_en);
 uint8_t reg_read(uint32_t *data, uint8_t addr, uint8_t csum_en);
 uint8_t page_select(uint8_t page, uint8_t csum_en);
 uint8_t instruction(uint8_t instruct_code, uint8_t csum_en);
 uint8_t reset(uint8_t csum_en);
+uint8_t start_conversion(uint8_t conversion_type, uint8_t csum_en, int timeout);
+uint32_t get_voltage_peak(uint8_t input_channel, uint8_t csum_en);
+uint32_t get_voltage_rms(uint8_t input_channel, uint8_t csum_en);
+uint32_t get_current_peak(uint8_t input_channel, uint8_t csum_en);
+uint32_t get_current_rms(uint8_t input_channel, uint8_t csum_en);
+uint32_t get_power_avg(uint8_t input_channel, uint8_t csum_en);
+
+
 
 #endif
